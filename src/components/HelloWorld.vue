1bdd93282f74d8eabb316acd189e1c112a5bba4e
tuananh08111 <tuananhdt16.1211@gmail.com>
2022-03-29 12:05:04 +0700

    DemoVue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+data(){
+  return{
+   count: 0
+    }
+  },
+  methods:{
+    increment(){
+      this.count++
+    }
+  }
+
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{count}}</button>
+</template>
+
+<style >
+.title{
+  font-size: 30px;
+  color:aqua;
+}
+
+</style>
